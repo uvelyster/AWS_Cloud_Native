024d079a79ba54b39344875f4350645a345700a7
uvelyster <48861250+uvelyster@users.noreply.github.com>
2024-05-01 23:05:40 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -247,12 +247,8 @@ resource "aws_lb_listener" "http" {
   port = 80
   protocol = "HTTP"
   default_action {
-    type = "fixed-response" 
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code = 404
-    }
+    type = "forward" 
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 
