40739170e354fb482f7e0b3db2fb36268cb140a3
uvelyster <48861250+uvelyster@users.noreply.github.com>
2024-05-01 22:02:17 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -275,15 +275,15 @@ resource "aws_lb_target_group" "asg" {
   port = 80
   protocol = "HTTP"
   vpc_id = aws_vpc.my_vpc.id
-  # health_check {
-  #   path = "/"
-  #   protocol = "HTTP"
+  health_check {
+    path = "/"
+    protocol = "HTTP"
   #   matcher = "200"
   #   interval = 15
   #   timeout = 3
   #   healthy_threshold = 2
   #   unhealthy_threshold = 2
-  # }
+  }
 }
 
 output "elb_dns_name" {
